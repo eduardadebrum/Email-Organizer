f81549f923a5a1ac79f75fe1b48daecd7f5924f5
Eduarda <eduardadebrum94@gmail.com>
2020-06-08 23:08:55 -0300

    Create Project

---
+++ src/main/java/br/com/eduarda/brum/email/organizer/EmailOrganizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduarda.brum.email.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eduarda/brum/email/organizer/EmailOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduarda.brum.email.organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
